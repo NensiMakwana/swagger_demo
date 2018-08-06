20e537c87dcc24523a7629818be76c30d0bbd418
NensiMakwana <nensi.makwana@bacancytechnology.com>
2018-08-06 14:27:49 +0530

    capistarano with pg

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20180130064456) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
